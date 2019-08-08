5987b29c90b1a865ac8f25e34feb6d0e9ac85bed
yujichino <chino@jsl.co.jp>
2019-08-09 00:03:48 +0900

    センサの種類を追加

--- README.md
+++ README.md
@@ -27,3 +27,39 @@ Raspberry Pi, SORACOMを使っての
 - [pppd](https://qiita.com/CLCL/items/95693f6a8daefc73ddaa)
 
 ## Usage
+
+### カメラモジュールを使用する
+
+[Raspberry Pi Camera Module V2 カメラモジュール (Daylight - element14)](https://www.amazon.co.jp/gp/product/B01ER2SKFS/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1)
+
+```bash
+# カメラモジュールを有効化
+$ sudo raspi-config
+
+「5 Interfacing Options」->「P1 Camera」->「Yes」を選択
+再起動
+
+# 認識されているかチェック
+$ vcgencmd get_camera
+supported=1 detected=1
+
+両方「1」になっていればOK
+
+# 撮影テスト
+$ sudo raspistill -o image.jpg
+```
+
+### GrovePi
+
+Github : https://github.com/DexterInd/GrovePi
+
+```
+# Install
+$ pwd
+/home/pi
+$ curl -kL dexterindustries.com/update_grovepi | bash
+...
+
+$ cd Firmware
+bash firmware_update.sh
+```
--- main_environment.py
+++ main_environment.py
@@ -3,6 +3,7 @@ import time
 import paho.mqtt.client as mqtt
 from datetime import datetime
 
+from utils.grove_pi import GrovePi
 from utils.unix_time import UnixTime
 
 host = 'beam.soracom.io'
@@ -14,9 +15,15 @@ client = mqtt.Client(protocol=mqtt.MQTTv311)
 try:
 
     # 温度、湿度を取得
-    # temp, humi = GrovePi.get_temp_humi()
-    # 照度を取得
-    # lux = GrovePi.get_lux()
+    temp, humi = GrovePi.get_temp_humi()
+    # 照度を取得 ( http://wiki.seeedstudio.com/Grove-Digital_Light_Sensor/ )
+    lux = GrovePi.get_lux()
+    # Loudness Sensor ( http://wiki.seeedstudio.com/Grove-Loudness_Sensor/ )
+    loudness_pi_mode = 0
+    loudness = GrovePi.analog_read(loudness_pi_mode)
+    # Air Quality Sensor ( http://wiki.seeedstudio.com/Grove-Air_Quality_Sensor_v1.3/ )
+    air_pi_mode = 1
+    air_quality = GrovePi.analog_read(air_pi_mode)
     # 水分量を取得
     # moisture = GrovePi.get_moisture()
 
@@ -26,10 +33,14 @@ try:
 
     # センシング情報を送信
     json_dict = {
-        'timestamp': UnixTime.date_time2unix_time(now)
+        'timestamp': UnixTime.date_time2unix_time(now),
+        'temp': temp,
+        'humi': humi,
+        'lux': lux,
+        'loudness': loudness,
+        'air_quality': air_quality,
     }
     result = client.publish(topic, json.dumps(json_dict), qos=1)
-    print(result)
 
 except Exception as e:
     print(e)
